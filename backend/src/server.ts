c01edc5ba45e0a3a80b1bac42850554d5c88d301
SibusisoGwebs <madonselasibusiso98@gmail.com>
2023-05-04 14:09:13 +0200

    Wildcard handler is removed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,9 +32,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
 
 //Database Related
 sequelize.authenticate().then(() => {
