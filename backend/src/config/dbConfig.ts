aee33959f56f834139438b83f6c9c455093b2202
SibusisoGwebs <madonselasibusiso98@gmail.com>
2023-05-17 16:13:22 +0200

    Another DB Changes

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -2,16 +2,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const database = {
-    host: process.env.HOST || '192.168.10.253',
-    user: process.env.USER || 'afsol_connector',
-    password: process.env.PASSWORD || 'afsol@afsol98',
-    db: process.env.DB || 'monitoring_database',
+    host: '192.168.10.253',
+    user: 'afsol_connector',
+    password: 'afsol@afsol98',
+    db: 'monitoring_database',
     dialect: process.env.DIALECT || 'mysql',
     dialectOptions: {
         useUTC: false,
     },
     timezone: '+02:00'
 }
+
+// export const database = {
+//     host: process.env.HOST || '192.168.10.253',
+//     user: process.env.USER || 'afsol_connector',
+//     password: process.env.PASSWORD || 'afsol@afsol98',
+//     db: process.env.DB || 'monitoring_database',
+//     dialect: process.env.DIALECT || 'mysql',
+//     dialectOptions: {
+//         useUTC: false,
+//     },
+//     timezone: '+02:00'
+// }
 
 export const databas = {
     host: 'localhost',
